4145ec0b302ae2e957e1ffaa013f10f8f4a7bb50
sallieeky <sallieeky@gmail.com>
2022-06-12 12:42:15 +0800

    revisi

--- resources/views/memberikan-pengesahan-tanda-tangan.blade.php
+++ resources/views/memberikan-pengesahan-tanda-tangan.blade.php
@@ -76,7 +76,7 @@
 
                   <div class="col-md-12 m-auto text-center">
                     <p>Mengetahui, <br></p>
-                    <img src="/img/ttd_wrba.jpeg" alt="">
+                    <img src="/img/ttd_wrba.jpeg" style="margin-left: -50px" alt="">
                   </div> 
                 </div>
               </section>
@@ -86,33 +86,6 @@
               <button type="button" id="cmd" class="btn btn-primary w-100">
                 Kirim Pengesahan Tanda Tangan
               </button>
-
-
-              {{-- <h5>Data Mahasiswa</h5>
-              <div class="form-group">
-                <label for="surat">Surat Cuti</label><br>
-                <a href="{{ env("API_URL") }}/variable-instance/{{ $data["file"][0]["id"] }}/data" target="_blank" class="btn btn-primary w-100">
-                  <i class="fas fa-file-pdf"></i>
-                  Surat Cuti Mahasiswa
-                </a>
-              </div>
-              <br> --}}
-
-              {{-- <form action="/memberikan-pengesahan-tanda-tangan" method="POST" enctype="multipart/form-data">
-                @csrf
-                <input type="hidden" name="id" value="{{ $data["task"][0]["id"] }}">
-                <input type="hidden" id="nama" name="nama" value="{{ $data["variable"][2]["value"] }}">
-                <input type="hidden" id="nim" name="nim" value="{{ $data["variable"][1]["value"] }}">
-                <input type="hidden" id="email" name="email" value="{{ $data["variable"][4]["value"] }}">
-                <input type="hidden" id="prodi" name="prodi" value="{{ $data["variable"][3]["value"] }}">
-                <input type="hidden" id="processInstanceId" name="processInstanceId" value="{{ $data["task"][0]["processInstanceId"] }}">
-                <h5>Tanda Tangan Pengesahan {{ $data["task"][0]["assignee"] }}</h5>
-                <div class="form-group">
-                  <label for="tanda_tangan_pengesahan">File Tanda Tangan Pengesahan {{ $data["task"][0]["assignee"] }}</label>
-                  <input type="file" id="tanda_tangan_pengesahan" required class="form-control" name="tanda_tangan_pengesahan" accept=".pdf">
-                </div>
-                <button type="submit" class="btn btn-primary">Kirim Pengesahan</button>
-              </form> --}}
 
             </div>
           </div>
